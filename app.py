6f1b1742d09fa93006ac3dfa99c7937039df7e7b
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2026-06-02 01:44:06 +0530

    Added JSON

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify, request
 app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template('base.html')
-@app.route('/tasks')
-def tasks():
-    return jsonify({'tasks':[
-        {
-            "id": 1,
-            "title": "Buy medicine",
-            "done": False
-        }
-    ]})
+applications=[]
+@app.route('/applications', methods=['POST'])
+def add_applications():
+    data=request.get_json()
+    applications.append(data)
+    return jsonify(data),201
+
 
 if __name__=='__main__':
     app.run(debug='True')
